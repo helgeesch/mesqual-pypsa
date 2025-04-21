7164114fb96321e91c5f899a320dfcc5dfeafeba
helgestefan <on@hstes.de>
2025-04-21 13:28:28 +0200

    moves mappings to class level

--- mescal_pypsa/pypsa_flag_index.py
+++ mescal_pypsa/pypsa_flag_index.py
@@ -8,6 +8,25 @@ logger = get_logger(__name__)
 
 
 class PyPSAFlagIndex(FlagIndex):
+    VISUALIZATION_TYPE_MAPPING = {
+        'bus': VisualizationTypeEnum.Point,
+        'generator': VisualizationTypeEnum.Point,
+        'load': VisualizationTypeEnum.Point,
+        'transformer': VisualizationTypeEnum.Point,
+        'line': VisualizationTypeEnum.Line,
+        'link': VisualizationTypeEnum.Line,
+        # TODO: add storage units, other object types
+    }
+
+    TOPOLOGY_TYPE_MAPPING = {
+        'bus': TopologyTypeEnum.Node,
+        'load': TopologyTypeEnum.NodeConnectedElement,
+        'generator': TopologyTypeEnum.NodeConnectedElement,
+        'transformer': TopologyTypeEnum.Edge,
+        'line': TopologyTypeEnum.Edge,
+        'link': TopologyTypeEnum.Edge,
+        # TODO: add storage units, other object types
+    }
 
     @classmethod
     def get_flag_type(cls) -> type[str]:
@@ -19,25 +38,17 @@ class PyPSAFlagIndex(FlagIndex):
     def _get_linked_model_flag(self, flag: str) -> str:
         if self._get_item_type(flag) != ItemTypeEnum.TimeSeries:
             raise ValueError
-        return flag.replace('_t', '')
+        return flag.split('.')[0].replace('_t', '')
 
     def _get_item_type(self, flag: str) -> ItemTypeEnum:
-        if flag.endswith('_t'):
+        object_class = flag.split('.')[0]
+        if object_class.endswith('_t'):
             return ItemTypeEnum.TimeSeries
         return ItemTypeEnum.Model
 
     def _get_visualization_type(self, flag: str) -> VisualizationTypeEnum:
         f = flag.lower()
-        types = {
-            'bus': VisualizationTypeEnum.Point,
-            'generator': VisualizationTypeEnum.Point,
-            'load': VisualizationTypeEnum.Point,
-            'transformer': VisualizationTypeEnum.Point,
-            'line': VisualizationTypeEnum.Line,
-            'link': VisualizationTypeEnum.Line,
-            # TODO: add storage units, other object types
-        }
-        for k, t in types.items():
+        for k, t in self.VISUALIZATION_TYPE_MAPPING.items():
             if k in f:
                 return t
         logger.warning(f'No VisualizationTypeEnum registered for {flag}. Falling back to "Other".')
@@ -45,23 +56,16 @@ class PyPSAFlagIndex(FlagIndex):
 
     def _get_topology_type(self, flag: str) -> TopologyTypeEnum:
         f = flag.lower()
-        types = {
-            'bus': TopologyTypeEnum.Node,
-            'load': TopologyTypeEnum.NodeConnectedElement,
-            'generator': TopologyTypeEnum.NodeConnectedElement,
-            'transformer': TopologyTypeEnum.Edge,
-            'line': TopologyTypeEnum.Edge,
-            'link': TopologyTypeEnum.Edge,
-            # TODO: add storage units, other object types
-        }
-        for k, t in types.items():
+        for k, t in self.TOPOLOGY_TYPE_MAPPING.items():
             if k in f:
                 return t
         logger.warning(f'No TopologyTypeEnum registered for {flag}. Falling back to "Other".')
         return TopologyTypeEnum.Other
 
     def _get_unit(self, flag: str) -> Units.Unit:
-        pass
+        if 'marginal_price' in flag:
+            return Units.EUR_per_MWh
+        return Units.NaU
 
     def _get_linked_model_flag_for_membership_column(self, membership_column_name: str) -> str:
         return to_plural(membership_column_name)
